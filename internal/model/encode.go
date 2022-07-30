817abc9e98b373e16a9ceb311503e00dcc7a708f
Mohammadmehdi Teymorian <mohammadmehdi.teymorian@snapp.cab>
2022-07-30 11:29:19 +0430

    feat: add encode data model

---
+++ internal/model/encode.go
@@ -0,0 +1,25 @@
+package model
+
+import "strings"
+
+type Encode struct {
+	Algorithm      string
+	Expiration     string
+	PublicKeyPath  string
+	PrivateKeyPath string
+	PayloadStr     string
+	Payload        map[string]any
+}
+
+func (e *Encode) Execute() {
+	payload := make(map[string]any)
+
+	fields := strings.Split(e.PayloadStr, "\n")
+
+	for _, field := range fields {
+		keyValue := strings.Split(field, ":")
+		payload[strings.TrimSpace(keyValue[0])] = strings.TrimSpace(keyValue[1])
+	}
+
+	e.Payload = payload
+}
