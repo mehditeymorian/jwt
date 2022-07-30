f066d92f839f64bdab795918cee632f8c0bd23e8
Mohammadmehdi Teymorian <mehditeymorian322@gmail.com>
2022-07-30 12:03:59 +0430

    feat: add jwt encode

---
+++ internal/jwt/jwt.go
@@ -0,0 +1,38 @@
+package jwt
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"time"
+
+	"jwt-cli/internal/model"
+
+	"github.com/golang-jwt/jwt/v4"
+)
+
+func Encode(encode model.Encode) (string, error) {
+	publicKeyFile, err := os.Open(encode.PublicKeyPath)
+	if err != nil {
+		return "", fmt.Errorf("failed to open public key file: %w", err)
+	}
+
+	signingKey, err := ioutil.ReadAll(publicKeyFile)
+	if err != nil {
+		return "", fmt.Errorf("failed to read public key: %w", err)
+	}
+
+	exp, _ := time.ParseDuration(encode.Expiration)
+
+	claims := jwt.MapClaims{
+		"iss":  encode.Issuer,
+		"exp":  time.Now().Add(exp).Unix(),
+		"data": encode.Payload,
+	}
+
+	token := jwt.NewWithClaims(jwt.GetSigningMethod(encode.Algorithm), claims)
+
+	signedString, err := token.SignedString(signingKey)
+
+	return signedString, fmt.Errorf("failed to sign claims with key: %w", err)
+}
