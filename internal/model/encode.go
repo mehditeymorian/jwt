eac686236840083e814194ddab0d5ecde97f517a
Mehdi Teymorian <mehditeymorian322@gmail.com>
2022-08-27 21:34:41 +0430

    feat: add standard fields for token encoding

--- internal/model/encode.go
+++ internal/model/encode.go
@@ -1,25 +1,9 @@
 package model
 
-import "strings"
-
 type Encode struct {
-	Algorithm     string
-	Expiration    string
-	PublicKeyPath string
-	Issuer        string
-	PayloadStr    string
-	Payload       map[string]any
-}
-
-func (e *Encode) Execute() {
-	payload := make(map[string]any)
-
-	fields := strings.Split(e.PayloadStr, "\n")
-
-	for _, field := range fields {
-		keyValue := strings.Split(field, ":")
-		payload[strings.TrimSpace(keyValue[0])] = strings.TrimSpace(keyValue[1])
-	}
-
-	e.Payload = payload
+	Algorithm  string
+	Expiration string
+	Issuer     string
+	Subject    string
+	Audience   string
 }
