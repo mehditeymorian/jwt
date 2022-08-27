ed046ae9fda00a7ecfb48c4293aad6620a75d753
Mehdi Teymorian <mehditeymorian322@gmail.com>
2022-08-27 21:58:39 +0430

    feat: add hmac signing method

--- internal/config/signing_method.go
+++ internal/config/signing_method.go
@@ -4,10 +4,15 @@ type SigningMethod string
 
 const (
 	RSA  SigningMethod = "rsa"
-	HMac SigningMethod = "hmac"
+	HMAC SigningMethod = "hmac"
 )
 
 type Rsa struct {
-	PublicKey  string
-	PrivateKey string
+	PublicKey  string `koanf:"public_key"`
+	PrivateKey string `koanf:"private_key"`
+}
+
+type HMac struct {
+	Key           string `koanf:"key"`
+	Base64Encoded bool   `koanf:"base_64_encoded"`
 }
