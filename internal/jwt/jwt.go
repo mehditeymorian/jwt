77bf36b7bd3fe4c2bbd50078e75f0b240f28230c
Mehdi Teymorian <mehditeymorian322@gmail.com>
2022-08-27 22:13:57 +0430

    feat: add decode functionality

--- internal/jwt/jwt.go
+++ internal/jwt/jwt.go
@@ -1,12 +1,15 @@
 package jwt
 
 import (
+	"errors"
 	"fmt"
 	"time"
 
 	"github.com/golang-jwt/jwt/v4"
 	"github.com/mehditeymorian/jwt/internal/model"
 )
+
+var errInvalidSigningMethod = errors.New("signing method dosn't match with configuration")
 
 func Encode(encode model.Encode, key any) (string, error) {
 	exp, _ := time.ParseDuration(encode.Expiration)
@@ -26,4 +29,20 @@ func Encode(encode model.Encode, key any) (string, error) {
 	}
 
 	return signedString, nil
+}
+
+func Decode(strToken string, key any, signingMethod string) (*jwt.Token, error) {
+	token, err := jwt.Parse(strToken, func(token *jwt.Token) (interface{}, error) {
+		if token.Method.Alg() != signingMethod {
+			return nil, errInvalidSigningMethod
+		}
+
+		return key, nil
+	})
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse token: %w", err)
+	}
+
+	return token, nil
 }
