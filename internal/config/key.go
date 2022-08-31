8f21b16b4d116184b4ad9a02ff2dd442ee5cb20e
Mehdi Teymorian <mehditeymorian322@gmail.com>
2022-08-31 17:45:05 +0430

    feat: generate random key decode key is not present

--- internal/config/key.go
+++ internal/config/key.go
@@ -5,6 +5,7 @@ import (
 	"regexp"
 
 	"github.com/golang-jwt/jwt/v4"
+	keyGenerator "github.com/mehditeymorian/jwt/internal/key"
 	"github.com/pterm/pterm"
 )
 
@@ -14,15 +15,36 @@ func (c *Config) DecodeKey(algorithm string) any {
 
 	switch {
 	case matchAlgorithm("(R|P)S.*", algorithm):
-		key, err = jwt.ParseRSAPublicKeyFromPEM([]byte(c.Rsa.PublicKey))
+		temp := c.Rsa.PublicKey
+		if temp == "" {
+			pterm.Warning.Println("RSA key is not available, generating random rsa key")
+
+			temp, _ = keyGenerator.GenerateRsaKeys(2048)
+		}
+
+		key, err = jwt.ParseRSAPublicKeyFromPEM([]byte(temp))
 	case matchAlgorithm("HS.*", algorithm):
+		temp := c.Hmac.Key
+		if temp == "" {
+			pterm.Warning.Println("Hmac key is not available, generating random hmac key")
+
+			temp = string(keyGenerator.GenerateHmacKey(64, true))
+		}
+
 		if c.Hmac.Base64Encoded {
-			key, err = base64.StdEncoding.DecodeString(c.Hmac.Key)
+			key, err = base64.StdEncoding.DecodeString(temp)
 		} else {
-			key = []byte(c.Hmac.Key)
+			key = []byte(temp)
 		}
 	case matchAlgorithm("ES.*", algorithm):
-		key, err = jwt.ParseECPublicKeyFromPEM([]byte(c.Ecdsa.PublicKey))
+		temp := c.Ecdsa.PublicKey
+		if temp == "" {
+			pterm.Warning.Println("Ecdsa key is not available, generating random Ecdsa key")
+
+			key, _ = keyGenerator.GenerateEcdsaKeys("P256")
+		}
+
+		key, err = jwt.ParseECPublicKeyFromPEM([]byte(temp))
 	}
 
 	if err != nil {
