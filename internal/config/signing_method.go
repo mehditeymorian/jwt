7167c9d6356310c416073cad3083f0724fccde56
Mehdi Teymorian <mehditeymorian322@gmail.com>
2022-09-01 23:06:12 +0430

    feat: add yaml tag to key configs

--- internal/config/signing_method.go
+++ internal/config/signing_method.go
@@ -9,16 +9,16 @@ const (
 )
 
 type Rsa struct {
-	PublicKey  string `koanf:"public_key"`
-	PrivateKey string `koanf:"private_key"`
+	PublicKey  string `koanf:"public_key" yaml:"public_key"`
+	PrivateKey string `koanf:"private_key" yaml:"private_key"`
 }
 
 type HMac struct {
-	Key           string `koanf:"key"`
-	Base64Encoded bool   `koanf:"base64_encoded"`
+	Key           string `koanf:"key" yaml:"key"`
+	Base64Encoded bool   `koanf:"base64_encoded" yaml:"base64_encoded"`
 }
 
 type Ecdsa struct {
-	PublicKey  string `koanf:"public_key"`
-	PrivateKey string `koanf:"private_key"`
+	PublicKey  string `koanf:"public_key" yaml:"public_key"`
+	PrivateKey string `koanf:"private_key" yaml:"private_key"`
 }
