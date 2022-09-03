079649d633d7d1cd54faaa20ffb8636f6a2dfe09
Mehdi Teymorian <mehditeymorian322@gmail.com>
2022-09-03 15:57:08 +0430

    feat: add set interactive

--- internal/cmd/set/set.go
+++ internal/cmd/set/set.go
@@ -1,6 +1,14 @@
 package set
 
-import "github.com/spf13/cobra"
+import (
+	"strconv"
+	"strings"
+
+	"github.com/mehditeymorian/jwt/internal/cmd"
+	"github.com/mehditeymorian/jwt/internal/config"
+	"github.com/pterm/pterm"
+	"github.com/spf13/cobra"
+)
 
 func Command() *cobra.Command {
 	c := &cobra.Command{
@@ -13,6 +21,30 @@ func Command() *cobra.Command {
 	return c
 }
 
-func set(cmd *cobra.Command, args []string) {
+func set(c *cobra.Command, args []string) {
+	configPath := cmd.GetConfigPath(c)
 
+	cfg := config.Load(configPath)
+
+	if len(args) < 2 {
+		pterm.Fatal.Println("not enough argument")
+	}
+
+	keys := strings.Split(args[0], ".")
+
+	switch keys[0] {
+	case "interactive":
+		setInteractive(cfg, args)
+	}
+
+	cfg.Save()
+}
+
+func setInteractive(cfg *config.Config, args []string) {
+	interactive, err := strconv.ParseBool(args[1])
+	if err != nil {
+		pterm.Fatal.Println("failed to parse interactive value")
+	}
+
+	cfg.Interactive = interactive
 }
